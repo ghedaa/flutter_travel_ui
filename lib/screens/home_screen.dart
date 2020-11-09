7b52918ff144a2d3e8d079f1e3c29e402ff57cde
ghedaa <ghedaa.aj@gmail.com>
2020-11-09 21:09:28 +0300

    9/11

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,43 +3,47 @@ import 'package:flutter_travel_ui/widgets/destination_carousel.dart';
 import 'package:flutter_travel_ui/widgets/hotel_carousel.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'package:flutter/cupertino.dart';
+
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-  int _currentTab = 0;
-  List<IconData> _icons = [
+  int slectedIndex=0;
+  int _currentTab=0;
+  List<IconData> icons=[
     FontAwesomeIcons.plane,
     FontAwesomeIcons.bed,
     FontAwesomeIcons.walking,
     FontAwesomeIcons.biking,
   ];
-
-  Widget _buildIcon(int index) {
+  Widget buildIcon (int index) {
     return GestureDetector(
-      onTap: () {
+      onTap:(){
         setState(() {
-          _selectedIndex = index;
+          slectedIndex=index;
+
         });
       },
       child: Container(
         height: 60.0,
         width: 60.0,
         decoration: BoxDecoration(
-          color: _selectedIndex == index
-              ? Theme.of(context).accentColor
-              : Color(0xFFE7EBEE),
+          color: slectedIndex== index
+              ?Theme.of(context).accentColor
+          :Color(0xFFE7EBEE),
           borderRadius: BorderRadius.circular(30.0),
         ),
         child: Icon(
-          _icons[index],
+          icons[index],
           size: 25.0,
-          color: _selectedIndex == index
-              ? Theme.of(context).primaryColor
-              : Color(0xFFB4C1C4),
+          color: slectedIndex==index? Theme
+              .of(context)
+              .primaryColor
+          :Color(0xFFB4C1C4),
         ),
       ),
     );
@@ -48,69 +52,71 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.symmetric(vertical: 30.0),
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 20.0, right: 120.0),
-              child: Text(
-                'What would you like to find?',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
+          children:<Widget> [
+        Padding(
+          padding: EdgeInsets.only(left: 20.0,right: 120.0),
+          child: Text('what would you like to find',style: TextStyle(fontSize: 30.0,fontWeight:FontWeight.bold,
+          ),),
+        ),
             SizedBox(height: 20.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: _icons
-                  .asMap()
-                  .entries
-                  .map(
-                    (MapEntry map) => _buildIcon(map.key),
-                  )
-                  .toList(),
-            ),
+              children: icons.asMap().entries.map(
+                  (MapEntry map)=> buildIcon(map.key),
+              ).toList(),
+
+              ),
             SizedBox(height: 20.0),
             DestinationCarousel(),
-            SizedBox(height: 20.0),
+            SizedBox(height: 20.0,),
             HotelCarousel(),
-          ],
-        ),
-      ),
+      ],
+    ),
+    ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentTab,
-        onTap: (int value) {
+      onTap: (int value){
           setState(() {
-            _currentTab = value; 
+            _currentTab=value;
           });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.search,
-              size: 30.0,
-            ),
-            title: SizedBox.shrink(),
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.search,
+            size: 30.0,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.local_pizza,
-              size: 30.0,
+      title: SizedBox.shrink(),
+        ),
+
+      BottomNavigationBarItem(
+        icon: Icon(
+          Icons.local_pizza,
+          size: 30.0,
+        ),
+        title: SizedBox.shrink(),
+
+        ),
+
+        BottomNavigationBarItem(
+          icon: CircleAvatar(
+            radius: 15.0,
+            backgroundImage: NetworkImage('http://i.imgur.com/zL4Krbz.jpg'
+
             ),
-            title: SizedBox.shrink(),
           ),
-          BottomNavigationBarItem(
-            icon: CircleAvatar(
-              radius: 15.0,
-              backgroundImage: NetworkImage('http://i.imgur.com/zL4Krbz.jpg'),
-            ),
-            title: SizedBox.shrink(),
-          )
-        ],
-      ),
+          title: SizedBox.shrink(),
+
+        )
+
+      ],
+    ),
     );
+
+
   }
 }
